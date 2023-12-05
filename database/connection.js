416e46e9303affe93d04097f14f669b16d9f08af
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-12-05 15:12:59 +0200

    update instance of database

--- database/connection.js
+++ database/connection.js
@@ -2,16 +2,16 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
+const sequelize = new Sequelize({
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
     dialect: process.env.DATABASE_DIALECT,
     port: process.env.DATABASE_PORT,
     protocol: 'postgres',
-    logging:  true
+    // logging:  true,
+    ssl: true
   }
 );
 
