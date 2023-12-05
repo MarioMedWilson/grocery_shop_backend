c1b5cb00a030d6531b296f73d378d33797fd3a15
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-12-05 15:28:55 +0200

    update instance of database

--- database/connection.js
+++ database/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize({
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
+    dialect: process.env.DATABASE_DIALECT,
     port: process.env.DATABASE_PORT,
     ssl: true
   }
