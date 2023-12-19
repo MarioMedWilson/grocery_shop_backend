0f003b695368c7cefea082a6186a057271c66088
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-12-19 23:19:00 +0200

    fixing error

--- routes/routes.js
+++ routes/routes.js
@@ -11,4 +11,8 @@ app.use('/', userRouter);
 app.get('/countries', countries.countriesData);
 app.get('/products', product.showProducts);
 
+app.use((req, res, next) => {
+  return res.status(404).json({ message: 'Endpoint not found' });
+});
+
 export default app;
