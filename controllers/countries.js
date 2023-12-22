e8060e8a2f54277115f9f972429db6ac223cef07
Youssef Zaghloul <youssef.zaghloul@ejust.edu.eg>
2023-12-23 01:37:21 +0200

    Update countries.js

--- controllers/countries.js
+++ controllers/countries.js
@@ -2,10 +2,20 @@ import client from "../database/connection.js";
 
 const countriesData = async (req, res) => {
   try {
-    const countries = await client.query(`SELECT * FROM brand_nationalities`);
-    return res.status(200).json(countries.rows);
+    // Retrieve countries data from the database
+    const result = await client.query(`SELECT * FROM brand_nationalities`);
+    
+    // Extract the rows from the result
+    const countries = result.rows;
+
+    // Respond with the countries' data
+    return res.status(200).json(countries);
   } catch (error) {
+    
+    // Log the error for debugging purposes
     console.error("Error getting countries:", error);
+
+    // Respond with a standardized error message
     return res.status(500).json({ error: "Internal Server Error" });
   }
 }
@@ -13,3 +23,4 @@ const countriesData = async (req, res) => {
 export default {
   countriesData
 };
+
