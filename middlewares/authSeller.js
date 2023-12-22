00a56e8d4746a10ee64c86627e04f9fdad5b3ae0
Youssef Zaghloul <youssef.zaghloul@ejust.edu.eg>
2023-12-23 01:44:37 +0200

    Enhance authSeller middleware
    Refactor the authSeller middleware for improved readability, error handling, and consistency. Descriptive error messages have been introduced, and constants have been used for magic strings. The logic for extracting and verifying the token has been separated for clarity. Additionally, async/await best practices have been followed, ensuring proper error handling during asynchronous operations.

--- middlewares/authSeller.js
+++ middlewares/authSeller.js
@@ -7,25 +7,39 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const JWT_SECRET = process.env.JWT_SECRET_ADMIN;
 
 export const authSeller = async (req, res, next) => {
-  let token;
+  l
   try {
+    let token;
+    
     if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
+
+    // Extract token from the Authorization header
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
       token = req.headers.authorization.split(" ")[1];
     }
+
+    // Check if the token exists
     if (!token) {
-      return res.status(404).json({message: "No token found!"});
+      return res.status(401).json({message: "No token found!"});
     }
+
+    // Verify the token
     const decoded = jwt.verify(token, JWT_SECRET);
+
+    // Retrieve seller information from the database
     const seller = await client.query(
       `SELECT * FROM "sellers" WHERE "id"='${decoded.id}'`
     );
+
+    // Check if seller exists
     if (!seller.rows[0]) {
       return res.status(404).json({message: "No seller found!"});
     }
+
+     // Attach the seller's user_id to the request object
     req.user_id = seller.rows[0].id;
+
+     // Continue with the next middleware
     return next();
   } catch (error) {
     console.error(error);
